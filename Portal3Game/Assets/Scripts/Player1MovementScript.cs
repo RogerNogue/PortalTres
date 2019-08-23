746d412fa3272cca5cc300c343db347b6907b8df
RogerNogue <rogernogueb@gmail.com>
2019-08-23 23:10:33 +0200

    made direction adaptation of the players

--- Portal3Game/Assets/Scripts/Player1MovementScript.cs
+++ Portal3Game/Assets/Scripts/Player1MovementScript.cs
@@ -6,32 +6,59 @@ public class Player1MovementScript : MonoBehaviour
 {
     public float playerSpeed;
 
+    private Vector3 orientationPosition;
+    private bool moving;
+
+    void calculateMovementAndDirection()
+    {        
+        //going through input detection modifying the position of the player
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0.0f, playerSpeed * Time.deltaTime, 0.0f);
+            orientationPosition.y += 1;
+            moving = true;
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+            orientationPosition.x -= 1;
+            moving = true;
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f);
+            orientationPosition.y -= 1;
+            moving = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+            orientationPosition.x += 1;
+            moving = true;
+        }
+        if(moving)
+        {
+  
+            transform.rotation = Quaternion.LookRotation(new Vector3(0.0f, 0.0f, -1.0f), new Vector3(-orientationPosition.y, orientationPosition.x, orientationPosition.z));
+
+            orientationPosition.x = orientationPosition.y = orientationPosition.z = 0.0f;
+            moving = false;
+
+            //maybe later need for lerping: https://answers.unity.com/questions/36255/lookat-to-only-rotate-on-y-axis-how.html
+        }
+
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        orientationPosition = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //going through input detection modifying the position of the player
-        if (Input.GetKey("w"))
-        {
-            transform.position += new Vector3(0.0f, playerSpeed * Time.deltaTime, 0.0f);
-        }
-        if (Input.GetKey("a"))
-        {
-            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
-        }
-        if (Input.GetKey("s"))
-        {
-            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f); 
-        }
-        if (Input.GetKey("d"))
-        {
-            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
-        }
+        calculateMovementAndDirection();
 
     }
 }
--- Portal3Game/Assets/Scripts/Player2MovementScript.cs
+++ Portal3Game/Assets/Scripts/Player2MovementScript.cs
@@ -5,33 +5,59 @@ using UnityEngine;
 public class Player2MovementScript : MonoBehaviour
 {
     public float playerSpeed;
+    private Vector3 orientationPosition;
+    private bool moving;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    void calculateMovementAndDirection()
     {
         //going through input detection modifying the position of the player
         if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.position += new Vector3(0.0f, playerSpeed * Time.deltaTime, 0.0f);
+            orientationPosition.y += 1;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+            orientationPosition.x -= 1;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.DownArrow))
         {
-            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f); 
+            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f);
+            orientationPosition.y -= 1;
+            moving = true;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += new Vector3(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+            orientationPosition.x += 1;
+            moving = true;
         }
+        if (moving)
+        {
+
+            transform.rotation = Quaternion.LookRotation(new Vector3(0.0f, 0.0f, -1.0f), new Vector3(-orientationPosition.y, orientationPosition.x, orientationPosition.z));
+
+            orientationPosition.x = orientationPosition.y = orientationPosition.z = 0.0f;
+            moving = false;
+
+            //maybe later need for lerping: https://answers.unity.com/questions/36255/lookat-to-only-rotate-on-y-axis-how.html
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        orientationPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        calculateMovementAndDirection();
 
     }
 }
