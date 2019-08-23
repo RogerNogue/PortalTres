d6e728b4cfabe1b97e6c92864cf9a456667e1f1d
Aniol <aniol92@hotmail.com>
2019-08-23 19:11:56 +0200

    Added mouseclicking

---
+++ Portal3Game/Assets/Scripts/MouseScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseScript : MonoBehaviour
+{
+    private Vector3 mousePos;
+    private Vector2 mousePos2D;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray2D = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit2D hit = Physics2D.GetRayIntersection(ray2D,Mathf.Infinity);
+            if (hit.collider != null)
+            {
+                Debug.Log(hit.collider.gameObject.transform.parent.gameObject.name);
+                SpriteRenderer spriteGO = hit.collider.gameObject.transform.parent.GetComponentInChildren<SpriteRenderer>();
+                spriteGO.color = Color.red;
+            }
+        }
+    }
+}
