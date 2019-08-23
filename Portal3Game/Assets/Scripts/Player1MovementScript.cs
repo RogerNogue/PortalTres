b5ba80f53741574e8b04cf48c8390b02de01c6c1
RogerNogue <rogernogueb@gmail.com>
2019-08-23 19:05:51 +0200

    new 2 prefabs of the players and a scene to test them

---
+++ Portal3Game/Assets/Scripts/Player1MovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1MovementScript : MonoBehaviour
+{
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //going through input detection modifying the position of the player
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0.0f, playerSpeed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f); 
+        }
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+    }
+}
---
+++ Portal3Game/Assets/Scripts/Player2MovementScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2MovementScript : MonoBehaviour
+{
+    public float playerSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //going through input detection modifying the position of the player
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0.0f, playerSpeed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += new Vector3(0, -playerSpeed * Time.deltaTime, 0.0f); 
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(playerSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+
+    }
+}
