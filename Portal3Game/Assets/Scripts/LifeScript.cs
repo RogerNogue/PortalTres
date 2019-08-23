c87dfd0f2e0e1db47919c7c99d89eace0faa0537
Aniol <aniol92@hotmail.com>
2019-08-23 23:57:50 +0200

    Added heart and lifescript

---
+++ Portal3Game/Assets/Scripts/LifeScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeScript : MonoBehaviour
+{
+    public GameObject firstHeart;
+    public GameObject secondHeart;
+    public GameObject thirdHeart;
+
+    public GameObject Player;
+
+    public int maxHP;
+
+    public int currentHP;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHP = maxHP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(currentHP > maxHP)
+        {
+            currentHP = maxHP;
+        }
+        else if(currentHP < 0)
+        {
+            //dead idk
+        }
+
+        switch(currentHP)
+        {
+            case 0:
+                firstHeart.SetActive(false);
+                break;
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+        }
+    }
+}
