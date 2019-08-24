1c7074a2335ec0cad0194aa9f1b1845a6979eab4
Aniol <aniol92@hotmail.com>
2019-08-24 18:12:49 +0200

    Added light png, GM prefab

--- Portal3Game/Assets/Scripts/GameManager.cs
+++ Portal3Game/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool cameraScrolling = false;
+    public bool cameraScrolling = true;
 
     public GameObject mainCamera;
 
