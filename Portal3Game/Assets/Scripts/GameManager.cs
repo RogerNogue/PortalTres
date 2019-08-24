3fcbb90a8d2ed73bc4af41930f0783eb87b739ab
Aniol <aniol92@hotmail.com>
2019-08-24 21:10:18 +0200

    Updated LevelTest with gameloop

--- Portal3Game/Assets/Scripts/GameManager.cs
+++ Portal3Game/Assets/Scripts/GameManager.cs
@@ -121,7 +121,7 @@ public class GameManager : MonoBehaviour
             P2win.SetActive(true);
             GameStop();
         }
-
+           
         if(GameFinished)
         {
             gameFinishTimer += Time.deltaTime;
