1c46e919cfb5f7788ca16716f0d0d6cdb2e48154
Alfonso <alfonsomestres@gmail.com>
2019-08-24 18:45:38 +0200

    Hearts are now in Canvas, Pause now stops camera and triggers police

--- Portal3Game/Assets/Scripts/GameManager.cs
+++ Portal3Game/Assets/Scripts/GameManager.cs
@@ -7,8 +7,9 @@ public class GameManager : MonoBehaviour
     public bool cameraScrolling = true;
 
     public GameObject mainCamera;
-
+    public SpriteRenderer redLight;
     private CameraScript camScript;
+    private bool paused = false;
 
     // Start is called before the first frame update
     void Start()
@@ -19,10 +20,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("c"))
-        {
-            cameraScrolling = !cameraScrolling;
-        }
+
         if (cameraScrolling)//input
         {
             camScript.enabled = true;
@@ -31,5 +29,22 @@ public class GameManager : MonoBehaviour
         {
             camScript.enabled = false;
         }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = !paused;
+            cameraScrolling = !paused;
+            redLight.enabled = paused;
+        }
+
+        if(paused)
+        {
+            // Freeze Players / change sprites / etc
+            // Trigger police sound
+            // enable redLight
+            // enable canvas
+            redLight.color = Color.Lerp(Color.white, Color.red, Mathf.PingPong(Time.time, 0.6f));
+        }
+
     }
 }
