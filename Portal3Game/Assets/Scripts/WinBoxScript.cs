6fa1e78f9770f05026dc67b5b2dc4558d9f710d8
Aniol <aniol92@hotmail.com>
2019-08-24 20:52:24 +0200

    Added winconditions

---
+++ Portal3Game/Assets/Scripts/WinBoxScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinBoxScript : MonoBehaviour
+{
+    public GameManager GameManagerScript;
+
+    private string winnerName;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        winnerName = col.transform.parent.name;
+        if(winnerName == "Homeless_1")
+        {
+            GameManagerScript.SetWin(1);
+        }
+        else if(winnerName == "Homeless_2")
+        {
+            GameManagerScript.SetWin(2);
+        }
+    }
+}
