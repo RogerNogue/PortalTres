65b3760e71c270175bad02618108b20ea43d208f
RogerNogue <rogernogueb@gmail.com>
2019-08-26 00:44:49 +0200

    wind emiter prefab and scene to test it done

---
+++ Portal3Game/Assets/Scripts/WindEmitterToggleScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEmitterToggleScript : MonoBehaviour
+{
+    void OnMouseDown()
+    {
+        transform.parent.gameObject.GetComponent<WindScript>().toggled();
+    }
+}
--- Portal3Game/Assets/Scripts/WindScript.cs
+++ Portal3Game/Assets/Scripts/WindScript.cs
@@ -10,43 +10,133 @@ public class WindScript : MonoBehaviour
     public float displacementDownwardsValue;
     public float displacementRightwardsValue;
     public float displacementLeftwardsValue;
+
+    public bool activated = true;
+
+    float currentDisplacementUpwardsValue;
+    float currentDisplacementDownwardsValue;
+    float currentDisplacementRightwardsValue;
+    float currentDisplacementLeftwardsValue;
+
+    Player1MovementScript pl1;
+    bool gotPlayer1 = false;
+    Player2MovementScript pl2;
+    bool gotPlayer2 = false;
+
+    void Start()
+    {
+        currentDisplacementUpwardsValue = displacementUpwardsValue;
+        currentDisplacementDownwardsValue = displacementDownwardsValue;
+        currentDisplacementRightwardsValue = displacementRightwardsValue;
+        currentDisplacementLeftwardsValue = displacementLeftwardsValue;
+    }
+
+    void addWindForceToAPlayer1(Player1MovementScript p)
+    {
+        p.upwardsDisplacement += currentDisplacementUpwardsValue;
+        p.downwardsDisplacement += currentDisplacementDownwardsValue;
+        p.leftwardsDisplacement += currentDisplacementLeftwardsValue;
+        p.rightwardsDisplacement += currentDisplacementRightwardsValue;
+    }
+    void addWindForceToAPlayer2(Player2MovementScript p)
+    {
+        p.upwardsDisplacement += currentDisplacementUpwardsValue;
+        p.downwardsDisplacement += currentDisplacementDownwardsValue;
+        p.leftwardsDisplacement += currentDisplacementLeftwardsValue;
+        p.rightwardsDisplacement += currentDisplacementRightwardsValue;
+    }
+
+    void substractWindForceToAPlayer1(Player1MovementScript p)
+    {
+        p.upwardsDisplacement -= currentDisplacementUpwardsValue;
+        p.downwardsDisplacement -= currentDisplacementDownwardsValue;
+        p.leftwardsDisplacement -= currentDisplacementLeftwardsValue;
+        p.rightwardsDisplacement -= currentDisplacementRightwardsValue;
+    }
+    void substractWindForceToAPlayer2(Player2MovementScript p)
+    {
+        p.upwardsDisplacement -= currentDisplacementUpwardsValue;
+        p.downwardsDisplacement -= currentDisplacementDownwardsValue;
+        p.leftwardsDisplacement -= currentDisplacementLeftwardsValue;
+        p.rightwardsDisplacement -= currentDisplacementRightwardsValue;
+    }
+
+    public void toggled()
+    {
+        activated = !activated;
+        if(activated)
+        {
+            turnedOn();
+            transform.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+        else
+        {
+            turnedOff();
+            transform.GetComponent<SpriteRenderer>().color = Color.blue;
+        }
+    }
+
+    void turnedOff()
+    {
+        //if any player was inside, change their stuff
+        if (gotPlayer1)
+        {
+            substractWindForceToAPlayer1(pl1);
+        }
+        if (gotPlayer2)
+        {
+            substractWindForceToAPlayer2(pl2);
+        }
+        currentDisplacementUpwardsValue = 0.0f;
+        currentDisplacementDownwardsValue = 0.0f;
+        currentDisplacementRightwardsValue = 0.0f;
+        currentDisplacementLeftwardsValue = 0.0f;
+    }
+    void turnedOn()
+    {
+        currentDisplacementUpwardsValue = displacementUpwardsValue;
+        currentDisplacementDownwardsValue = displacementDownwardsValue;
+        currentDisplacementRightwardsValue = displacementRightwardsValue;
+        currentDisplacementLeftwardsValue = displacementLeftwardsValue;
+        if (gotPlayer1)
+        {
+            addWindForceToAPlayer1(pl1);
+        }
+        if (gotPlayer2)
+        {
+            addWindForceToAPlayer2(pl2);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         //if player is invulnerable, we ignore the collision
         if (collision.transform.parent.tag == "Player1")
         {
-            Player1MovementScript player1 = collision.transform.parent.GetComponent<Player1MovementScript>();
-            player1.upwardsDisplacement += displacementUpwardsValue;
-            player1.downwardsDisplacement += displacementDownwardsValue;
-            player1.leftwardsDisplacement += displacementLeftwardsValue;
-            player1.rightwardsDisplacement += displacementRightwardsValue;
+            pl1 = collision.transform.parent.GetComponent<Player1MovementScript>();
+            addWindForceToAPlayer1(pl1);
+            gotPlayer1 = true;
         }
         if (collision.transform.parent.tag == "Player2")
         {
-            Player2MovementScript player2 = collision.transform.parent.GetComponent<Player2MovementScript>();
-            player2.upwardsDisplacement += displacementUpwardsValue;
-            player2.downwardsDisplacement += displacementDownwardsValue;
-            player2.leftwardsDisplacement += displacementLeftwardsValue;
-            player2.rightwardsDisplacement += displacementRightwardsValue;
+            pl2 = collision.transform.parent.GetComponent<Player2MovementScript>();
+            addWindForceToAPlayer2(pl2);
+            gotPlayer2 = true;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.transform.parent.tag == "Player1")
         {
-            Player1MovementScript player1 = collision.transform.parent.GetComponent<Player1MovementScript>();
-            player1.upwardsDisplacement -= displacementUpwardsValue;
-            player1.downwardsDisplacement -= displacementDownwardsValue;
-            player1.leftwardsDisplacement -= displacementLeftwardsValue;
-            player1.rightwardsDisplacement -= displacementRightwardsValue;
+            pl1 = collision.transform.parent.GetComponent<Player1MovementScript>();
+            substractWindForceToAPlayer1(pl1);
+            gotPlayer1 = false;
         }
         if (collision.transform.parent.tag == "Player2")
         {
-            Player2MovementScript player2 = collision.transform.parent.GetComponent<Player2MovementScript>();
-            player2.upwardsDisplacement -= displacementUpwardsValue;
-            player2.downwardsDisplacement -= displacementDownwardsValue;
-            player2.leftwardsDisplacement -= displacementLeftwardsValue;
-            player2.rightwardsDisplacement -= displacementRightwardsValue;
+            pl2 = collision.transform.parent.GetComponent<Player2MovementScript>();
+            substractWindForceToAPlayer2(pl2);
+            gotPlayer2 = false;
         }
     }
 }
